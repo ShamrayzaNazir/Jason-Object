077b9554262979ecb4fb4b5e0bd4820379d52275
Shamrayza Nazir <Binty.Sherry@gmail.com>
2024-07-08 23:34:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "sherry";
+console.log(name);
+// json format 
+// json data = "name" : "sherry"
+// json object = "name" : "sherry", "age" : "22", "gender" : "female",
+// json array = sai ni pta
+let info = {
+    "name": "sherry",
+    "age": "22",
+    "gender": "female",
+};
+console.log(info);
+console.log(info.name);
+console.log(info.age);
+console.log(info.gender);
---
+++ index.ts
@@ -0,0 +1,17 @@
+let name = "sherry";
+console.log(name);
+
+// json format 
+// json data = "name" : "sherry"
+// json object = "name" : "sherry", "age" : "22", "gender" : "female",
+// json array = sai ni pta
+let info ={
+    "name" : "sherry",
+    "age" : "22",
+    "gender" : "female",
+
+}
+console.log(info);
+console.log(info.name);
+console.log(info.age);
+console.log(info.gender);
